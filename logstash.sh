ba57ae8ef524ad163b78343980a5cfcca9e6e677
Petri Sirkkala <sirpete@iki.fi>
2016-03-16 11:15:18 +0200

    Major rewrite of main script

--- logstash.sh
+++ logstash.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# docker run -d -P --link elastic:es --name logstash-kibana pblittle/docker-logstash
-# 
-# 
-# 
-# docker run -d 
-# 
-# 
-# $ docker run -it --rm logstash logstash -e 'input { file { path => "/tmp/*.log" } } output { stdout { } }'
+# Run Elastic Search, Logstash and Kibana in Docker
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
 
-docker run -d --rm --link elastic:es -v "$PWD":/config-dir logstash logstash -f /config-dir/logstash.conf
+ESDATA=$BASEDIR/esdata
+LSCONF=$BASEDIR/conf
+LSDATA=$BASEDIR/data
 
+docker rm -f elasticsearch logstash >/dev/null 2>&1 # Clean up old containers
+
+  #-v "$ESDATA":/usr/share/elasticsearch/data/elasticsearch \
+docker run -d --name elasticsearch \
+  elasticsearch:2.2_no_data_volume
+
+docker run -d --name logstash \
+  --link elasticsearch:elasticsearch \
+  -v "$LSCONF":/config-dir \
+  -v "$LSDATA":/data \
+  logstash logstash -f /config-dir/logstash.conf
